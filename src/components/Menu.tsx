3cd1c15626f4f07d416951b8f60873d92624e0ef
Guilherme Dellagustin <dellagustin@gmail.com>
2020-10-04 20:05:02 +0200

    Converted spaces to tabs

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,13 @@
 import {
-  IonContent,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonMenu,
-  IonMenuToggle,
-  IonNote,
+	IonContent,
+	IonIcon,
+	IonItem,
+	IonLabel,
+	IonList,
+	IonListHeader,
+	IonMenu,
+	IonMenuToggle,
+	IonNote,
 } from '@ionic/react';
 
 import React from 'react';
@@ -16,55 +16,55 @@ import { searchOutline, searchSharp, homeOutline, homeSharp, gitBranchOutline } 
 import './Menu.css';
 
 interface AppPage {
-  url: string;
-  iosIcon: string;
-  mdIcon: string;
-  title: string;
-}
+	url: string;
+	iosIcon: string;
+	mdIcon: string;
+	title: string;
+	}
 
 const appPages: AppPage[] = [
-  {
-    title: 'Home',
-    url: '/page/Home',
-    iosIcon: homeOutline,
-    mdIcon: homeSharp
-  },
-  {
-    title: 'Search Podcast',
-    url: '/page/AddPodcast',
-    iosIcon: searchOutline,
-    mdIcon: searchSharp
-  },
-  /*{
-    title: 'Source Code',
-    url: 'https://github.com/podStation/podstation-ionic-pwa',
-    iosIcon: gitBranchOutline,
-    mdIcon: gitBranchOutline
-  },
-  */
+	{
+		title: 'Home',
+		url: '/page/Home',
+		iosIcon: homeOutline,
+		mdIcon: homeSharp
+	},
+	{
+		title: 'Search Podcast',
+		url: '/page/AddPodcast',
+		iosIcon: searchOutline,
+		mdIcon: searchSharp
+	},
+	/*{
+		title: 'Source Code',
+		url: 'https://github.com/podStation/podstation-ionic-pwa',
+		iosIcon: gitBranchOutline,
+		mdIcon: gitBranchOutline
+	},
+	*/
 ];
 
 const Menu: React.FC = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <IonMenu contentId="main" type="overlay">
-      <IonContent>
-        <IonList id="inbox-list">
-          {appPages.map((appPage, index) => {
-            return (
-              <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
-                  <IonLabel>{appPage.title}</IonLabel>
-                </IonItem>
-              </IonMenuToggle>
-            );
-          })}
-        </IonList>
-      </IonContent>
-    </IonMenu>
-  );
+	return (
+		<IonMenu contentId="main" type="overlay">
+			<IonContent>
+				<IonList id="inbox-list">
+				{appPages.map((appPage, index) => {
+					return (
+					<IonMenuToggle key={index} autoHide={false}>
+						<IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+						<IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+						<IonLabel>{appPage.title}</IonLabel>
+						</IonItem>
+					</IonMenuToggle>
+					);
+				})}
+				</IonList>
+			</IonContent>
+		</IonMenu>
+	);
 };
 
 export default Menu;
