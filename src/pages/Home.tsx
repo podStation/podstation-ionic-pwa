ed6bc02268f0c3544b970c6e79af2ef4bfde311f
Guilherme Dellagustin <dellagustin@gmail.com>
2020-10-08 22:41:05 +0200

    Added notice that it is not ready for use yet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,30 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonButton, IonText, IonTextarea } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 
 const Home: React.FC = () => {
+	const { name } = useParams<{ name: string; }>();
 
-  const { name } = useParams<{ name: string; }>();
+	return (
+	<IonPage>
+		<IonHeader>
+			<IonToolbar>
+				<IonButtons slot="start">
+					<IonMenuButton />
+				</IonButtons>
+				<IonTitle>Welcome to podStation</IonTitle>
+			</IonToolbar>
+		</IonHeader>
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>Welcome to podStation</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent fullscreen>
-        <p>podStation is planned to be a web application for subscribing and listening to podcasts.</p>
-        <p>As you can see, it is still under construction.</p>
-        <p>Come back from time to time to see how we are doing.</p>
-      </IonContent>
-    </IonPage>
-  );
+		<IonContent fullscreen>
+			<p>podStation is planned to be a web application for subscribing and listening to podcasts.</p>
+			<p>As you can see, it is still under construction.</p>
+			<p>Come back from time to time to see how we are doing.</p>
+			<p>Don't get to attached to any functionality or to your data, this may break or change, it is not ready for use yet</p>
+		</IonContent>
+	</IonPage>
+	);
 };
 
 export default Home;
